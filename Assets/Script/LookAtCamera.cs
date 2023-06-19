04c00896ba16c9142bbc4294e0acfe93e32fadf5
Franco Soler <francosolerlanderreche@gmail.com>
2023-06-19 20:59:31 -0300

    Materials and spotlight

--- Assets/Script/LookAtCamera.cs
+++ Assets/Script/LookAtCamera.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class LookAtCamera : MonoBehaviour
 {
-    public Camera Camera;
     public GameObject target;
     // Start is called before the first frame update
     void Start()
